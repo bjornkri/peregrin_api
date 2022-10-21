a70df70eefe9db1f7337b00d1444da0aa3744404
Björn Kristinsson <bjorn@bjornkri.com>
2022-10-21 14:49:14 +0200

    Add legacy readingupdate import script

---
+++ peregrin/core/utils.py
@@ -0,0 +1,25 @@
+
+import json
+from books.serializers import Book, ReadingUpdateSerializer
+
+
+def import_legacy(json_filename):
+    js = json.load(open(json_filename, 'r'))
+    for record in js:
+        if record['model'] == "books.book":
+            Book.objects.create(
+                id=record['pk'],
+                title=record['fields']['title'],
+                end_location=record['fields']['total'],
+                start_location=record['fields']['start_location'],
+                start_date=record['fields']['start_date'],
+            )
+        elif record['model'] == 'history.locationupdate':
+            update = ReadingUpdateSerializer(data={
+                    "book": record['fields']['book'],
+                    "current_page": record['fields']['location'],
+                    "date": record['fields']['date']
+                }
+            )
+            if update.is_valid():
+                update.save()
