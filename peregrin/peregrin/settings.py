851f68c589a6b2b84066c58fa745ce393fdb5a2d
Björn Kristinsson <bjorn@bjornkri.com>
2023-04-13 17:21:04 +0000

    Add postgres support

--- peregrin/peregrin/settings.py
+++ peregrin/peregrin/settings.py
@@ -95,12 +95,10 @@ WSGI_APPLICATION = 'peregrin.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(
+        'DATABASE_URL',
+        default='postgres:///peregrin_two'),
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
